4d48e015c4601a85c7e0c08220bf97059b5d4744
Radosław Chamot <radoslaw.chamot@schibsted.pl>
2014-11-19 15:06:57 +0100

    add histogram

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ from http://www.lfd.uci.edu/~gohlke/pythonlibs/ download and install:
 - Python-igraph
 - Pycairo 
 
+
+###Ubuntu
+ `sudo apt-get install build-essential python-dev`
+
+ `sudo pip install python-igraph`
+
+Histogram dependencies:
+ `sudo apt-get install libfreetype6-dev`
+
+ `sudo pip install matplotlib`
+
---
+++ SmallWorld/network_properties.py
@@ -0,0 +1,31 @@
+from Network import Network, readData
+from igraph import plot
+import matplotlib.pyplot as plt
+
+n = Network()
+data = readData("facebook_combined_short.txt", " ", n)
+g = n.toIGraph()
+
+visual_style = {}
+layout = g.layout("circle")
+
+visual_style["layout"] = layout
+visual_style["bbox"] = (6400, 3600)
+
+print "plotting evil"
+plot(g, **visual_style)
+
+print "Diameter:" + str(g.diameter())
+
+avg_path = g.average_path_length(directed=False)
+print "Avg path len:" + str(avg_path)
+print g.path_length_hist()
+
+plt.hist(g.closeness())
+plt.show()
+
+plt.hist(g.eccentricity())
+plt.show()
+
+plt.hist(g.betweenness())
+plt.show()
